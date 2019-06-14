a771b1b7f4b7104ecbcaf002e959cc308a33524f
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-06-14 22:56:44 +0200

    dodano treść do strony za pomocą skryptu JS

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
 const autor = 'Kasia Izak';
 
 console.log(`Hej, nazywam się ${autor} i jestem autorką tej strony.
-Fajnie, że zaglądasz do konsoli, ale czego się tu spodziewasz znaleźć? ;-)`)
+Fajnie, że zaglądasz do konsoli, ale czego się tu spodziewasz znaleźć? ;-)`);
+
+const footerParagraph = document.querySelector('.footer__paragraph--js');
+
+footerParagraph.innerHTML = `Stronę koduje: ${autor}`;
