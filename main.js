a0597a3fb7cd736487f600355f7e790085ef9805
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-06-21 13:50:29 +0200

    dodano funkcję "witającą" w konsoli

--- main.js
+++ main.js
@@ -6,3 +6,13 @@ Fajnie, że zaglądasz do konsoli, ale czego się tu spodziewasz znaleźć? ;-)`
 const footerParagraph = document.querySelector('.footer__paragraph--js');
 
 footerParagraph.innerHTML = `Stronę koduje: ${autor}`;
+
+function welcome(name,age) {
+  if (age < 18) {
+    console.log (`Witaj ${name}, widzę że do pełnoletności jeszcze trochę brakuje, ale to nie szkodzi! :-)`)
+  } else {
+    console.log (`Witaj ${name}, widzę że 18 lat to masz już skończone... :-)`)
+  }
+}
+
+welcome('Kasia', 36);
