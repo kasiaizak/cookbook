40888fd5f1acf03d5f4710d62cd801d9c8a47d24
Katarzyna Izak <katarzynaizak@users.noreply.github.com>
2019-07-03 13:27:09 +0200

    hamburger zamykany X'em

--- main.js
+++ main.js
@@ -23,4 +23,9 @@ const navigationSwitcher = document.querySelector('.navigation__switcher--js')
 navigationSwitcher.addEventListener('click', (e) => {
   const navigationList = document.querySelector('.navigation__list--js')
   navigationList.classList.toggle('navigation__list--visible');
+  if (navigationList.classList.contains('navigation__list--visible')) {
+    navigationSwitcher.innerHTML = 'x';
+  } else {
+    navigationSwitcher.innerHTML = '≡';
+  }
 });
